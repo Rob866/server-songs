ace0c339fa3bb824452ab35456a702c3e5893aec
robert <juanrob_10@hotmail.com>
2019-08-12 16:46:30 -0700

    boom

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
-const config = require('../config/config')
+// const config = require('../config/config')
 const db = {}
 
-const sequelize = new Sequelize(
-  config.db.database,
-  config.db.user,
-  config.db.password,
-  config.db.options
-)
+// const { database, user, password, options } = config.db
+const sequelize = new Sequelize('postgres://chpfkcqwqpiknp:8a1a108b73dfcdf9f128d0fe959563e3da3d1196abbea3a06b14f06bc31b415c@ec2-174-129-229-106.compute-1.amazonaws.com:5432/d15t4hekreld5m')
 
 fs
   .readdirSync(__dirname)
